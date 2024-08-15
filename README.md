d5a1b1fb99f4b8e2e512ee1bb739a74c9fa9ed88
root <root@nerf-zhy.cspmrt>
2024-08-15 12:35:34 +0000

    add ubuntu_config

---
+++ README.md
@@ -0,0 +1,3 @@
+# zhywytConfig 
+restore my config file
+`ubuntu.sh` can soft link the file to local config
---
+++ ubuntu.sh
@@ -0,0 +1,4 @@
+ln -s ~/.bashrc .bashrc
+ln -s /etc/ssh/ssh_config ssh_config
+ln -s /etc/vim/vimrc vimrc
+source ~/.bashrc
