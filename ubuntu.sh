e3f3ea0b79f15e7624b40b7bea50523ccc9372f6
root <root@nerf-zhy.cspmrt>
2024-08-17 06:05:16 +0000

    add copy of old file

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,15 @@
-ln -s ~/.bashrc .bashrc
-ln -s /etc/ssh/ssh_config ssh_config
-ln -s /etc/vim/vimrc vimrc
-ln -s ~/.ssh/id_ras.pub id_ras.pub
+cp ~/.bashrc ~/.bashrc.back
+cp /etc/ssh/ssh_config /etc/ssh/ssh_config.back
+cp /etc/vim/vimrc /etc/vim/vimrc.back
+cp ~/.ssh/id_ras.pub ~/.ssh/id_ras.pub.back
+
+rm ~/.bashrc 
+rm /etc/ssh/ssh_config
+rm /etc/vim/vimrc
+rm ~/.ssh/id_ras.pub
+
+ln -s $(pwd)/.bashrc ~/.bashrc 
+ln -s $(pwd)/ssh_config /etc/ssh/ssh_config  
+ln -s $(pwd)/vimrc /etc/vim/vimrc 
+ln -s $(pwd)/id_ras.pub ~/.ssh/id_ras.pub 
 source ~/.bashrc
