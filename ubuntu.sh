092da5fdc86ce9a29af39e5d13c0ff0c6ca36c61
root <root@pve.zhywyt>
2024-08-19 12:28:24 +0800

    fix ssh pub key name

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,15 +1,15 @@
 cp ~/.bashrc ~/.bashrc.back
 cp /etc/ssh/ssh_config /etc/ssh/ssh_config.back
 cp /etc/vim/vimrc /etc/vim/vimrc.back
-cp ~/.ssh/id_ras.pub ~/.ssh/id_ras.pub.back
+cp ~/.ssh/id_rsa.pub ~/.ssh/id_rsa.pub.back
 
 rm ~/.bashrc 
 rm /etc/ssh/ssh_config
 rm /etc/vim/vimrc
-rm ~/.ssh/id_ras.pub
+rm ~/.ssh/id_rsa.pub
 
 ln -s $(pwd)/.bashrc ~/.bashrc 
 ln -s $(pwd)/ssh_config /etc/ssh/ssh_config  
 ln -s $(pwd)/vimrc /etc/vim/vimrc 
-ln -s $(pwd)/id_ras.pub ~/.ssh/id_ras.pub 
+ln -s $(pwd)/id_rsa.pub ~/.ssh/id_rsa.pub 
 source ~/.bashrc
